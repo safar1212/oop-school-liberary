56360e3fc8cd5198f94d4a6db02c588b1297b421
safar ali <miqra019@gmail.com>
2022-08-24 10:05:24 +0500

    update the trimmer method

--- trimmer_decorator.rb
+++ trimmer_decorator.rb
@@ -7,6 +7,6 @@ class TrimmerDecorator < BaseDecorator
   end
 
   def correct_name
-    @nameable.correct_name
+    @nameable.correct_name.length > 10 ? @nameable.correct_name[0..9] : @nameable.correct_name
   end
 end
